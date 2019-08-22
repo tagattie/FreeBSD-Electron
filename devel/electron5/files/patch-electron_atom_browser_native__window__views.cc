500c86dcc8d80193a488141f7aa9a9dac6b5939f
Hiroki Tagato <tagattie@gmail.com>
2019-08-22 18:43:53 +0900

    electron5: update to v5.0.10.

--- devel/electron5/files/patch-electron_atom_browser_native__window__views.cc
+++ devel/electron5/files/patch-electron_atom_browser_native__window__views.cc
@@ -1,6 +1,6 @@
---- electron/atom/browser/native_window_views.cc.orig	2019-05-03 20:33:57 UTC
+--- electron/atom/browser/native_window_views.cc.orig	2019-08-19 21:30:51 UTC
 +++ electron/atom/browser/native_window_views.cc
-@@ -289,7 +289,7 @@ NativeWindowViews::NativeWindowViews(const mate::Dicti
+@@ -290,7 +290,7 @@ NativeWindowViews::NativeWindowViews(const mate::Dicti
    last_normal_bounds_ = GetBounds();
  #endif
  
@@ -9,7 +9,7 @@
    // Listen to move events.
    aura::Window* window = GetNativeWindow();
    if (window)
-@@ -305,7 +305,7 @@ NativeWindowViews::~NativeWindowViews() {
+@@ -306,7 +306,7 @@ NativeWindowViews::~NativeWindowViews() {
    SetForwardMouseMessages(false);
  #endif
  
@@ -18,7 +18,7 @@
    aura::Window* window = GetNativeWindow();
    if (window)
      window->RemovePreTargetHandler(this);
-@@ -1220,7 +1220,7 @@ void NativeWindowViews::OnWidgetBoundsChanged(views::W
+@@ -1228,7 +1228,7 @@ void NativeWindowViews::OnWidgetBoundsChanged(views::W
  }
  
  void NativeWindowViews::OnWidgetDestroying(views::Widget* widget) {
@@ -27,7 +27,7 @@
    aura::Window* window = GetNativeWindow();
    if (window)
      window->RemovePreTargetHandler(this);
-@@ -1313,7 +1313,7 @@ void NativeWindowViews::OnWidgetMove() {
+@@ -1321,7 +1321,7 @@ void NativeWindowViews::OnWidgetMove() {
  void NativeWindowViews::HandleKeyboardEvent(
      content::WebContents*,
      const content::NativeWebKeyboardEvent& event) {
@@ -36,7 +36,7 @@
    if (event.windows_key_code == ui::VKEY_BROWSER_BACK)
      NotifyWindowExecuteAppCommand(kBrowserBackward);
    else if (event.windows_key_code == ui::VKEY_BROWSER_FORWARD)
-@@ -1325,7 +1325,7 @@ void NativeWindowViews::HandleKeyboardEvent(
+@@ -1333,7 +1333,7 @@ void NativeWindowViews::HandleKeyboardEvent(
    root_view_->HandleKeyEvent(event);
  }
  
--- devel/electron5/files/patch-electron_spec_api-browser-window-spec.js
+++ devel/electron5/files/patch-electron_spec_api-browser-window-spec.js
@@ -1,6 +1,15 @@
---- electron/spec/api-browser-window-spec.js.orig	2019-07-23 18:38:39 UTC
+--- electron/spec/api-browser-window-spec.js.orig	2019-08-19 21:30:51 UTC
 +++ electron/spec/api-browser-window-spec.js
-@@ -1268,7 +1268,7 @@ describe('BrowserWindow module', () => {
+@@ -1174,7 +1174,7 @@ describe('BrowserWindow module', () => {
+   describe('BrowserWindow.setOpacity(opacity)', () => {
+     describe('Windows and Mac', () => {
+       before(function () {
+-        if (process.platform === 'linux') {
++        if (process.platform === 'linux' || process.platform === 'freebsd') {
+           this.skip()
+         }
+       })
+@@ -1306,7 +1306,7 @@ describe('BrowserWindow module', () => {
  
    describe('enableLargerThanScreen" option', () => {
      before(function () {
@@ -9,7 +18,7 @@
          this.skip()
        }
      })
-@@ -2362,7 +2362,7 @@ describe('BrowserWindow module', () => {
+@@ -2400,7 +2400,7 @@ describe('BrowserWindow module', () => {
        w.loadFile(path.join(fixtures, 'pages', 'visibilitychange.html'))
      })
      it('visibilityState changes when window is minimized', function (done) {
@@ -18,16 +27,7 @@
          // FIXME(alexeykuzmin): Skip the test instead of marking it as passed.
          // afterEach hook won't be run if a test is skipped dynamically.
          // If afterEach isn't run current window won't be destroyed
-@@ -2554,7 +2554,7 @@ describe('BrowserWindow module', () => {
-   describe('beginFrameSubscription method', () => {
-     before(function () {
-       // FIXME These specs crash on Linux when run in a docker container
--      if (isCI && process.platform === 'linux') {
-+      if (isCI && (process.platform === 'linux' || process.platform === 'freebsd')) {
-         this.skip()
-       }
-     })
-@@ -2800,7 +2800,7 @@ describe('BrowserWindow module', () => {
+@@ -2833,7 +2833,7 @@ describe('BrowserWindow module', () => {
      // - `.skip()` called in the 'beforeEach' hook prevents 'afterEach'
      //     hook from being called.
      // Not implemented on Linux.
