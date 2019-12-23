90618e8c40de9083cb785b3d0af547f5b7026742
Hiroki Tagato <tagattie@gmail.com>
2019-12-23 15:50:45 +0900

    electron7: add freebsd11 and freebsd13.

--- devel/electron7/files/patch-electron_script_lib_config.py
+++ devel/electron7/files/patch-electron_script_lib_config.py
@@ -1,10 +1,12 @@
---- electron/script/lib/config.py.orig	2019-12-17 05:39:13 UTC
+--- electron/script/lib/config.py.orig	2019-12-19 14:49:00 UTC
 +++ electron/script/lib/config.py
-@@ -21,6 +21,7 @@ BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
+@@ -21,6 +21,9 @@ BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
  PLATFORM = {
    'cygwin': 'win32',
    'darwin': 'darwin',
++  'freebsd11': 'freebsd',
 +  'freebsd12': 'freebsd',
++  'freebsd13': 'freebsd',
    'linux': 'linux',
    'linux2': 'linux',
    'win32': 'win32',
