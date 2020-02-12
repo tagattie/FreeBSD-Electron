3a1d50a1178f8ad04ed4725bf571f344bab85ccc
Hiroki Tagato <tagattie@gmail.com>
2020-02-12 19:52:25 +0900

    electron.mk: add MAKE_ENV to always use system 7za.

--- Mk/Uses/electron.mk
+++ Mk/Uses/electron.mk
@@ -264,6 +264,7 @@ BUILD_DEPENDS+=	npm:${NPM_PORTDIR}	# npm is needed for node-gyp
 
 MAKE_ENV+=	ELECTRON_SKIP_BINARY_DOWNLOAD=1 # effective electron >=6
 MAKE_ENV+=	SASS_FORCE_BUILD=true		# always rebuild native node-sass module
+MAKE_ENV+=	USE_SYSTEM_7ZA=true		# always use system 7za
 MAKE_ENV+=	USE_SYSTEM_APP_BUILDER=true	# always use system app-builder for electron-builder
 MAKE_ENV+=	XDG_CACHE_HOME=${WRKDIR}/.cache
 MAKE_ENV+=	npm_config_build_from_source=true
