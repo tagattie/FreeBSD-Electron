66f92b559b3b1839d9f17a033a9912e19b51c14a
Hiroki Tagato <tagattie@gmail.com>
2020-06-23 17:54:16 +0900

    electron8: fix window.navigator.onLine is always false.

--- devel/electron8/files/patch-net_base_network__change__notifier.cc
+++ devel/electron8/files/patch-net_base_network__change__notifier.cc
@@ -1,4 +1,4 @@
---- net/base/network_change_notifier.cc.orig	2020-03-03 07:03:11 UTC
+--- net/base/network_change_notifier.cc.orig	2020-05-02 08:42:14 UTC
 +++ net/base/network_change_notifier.cc
 @@ -35,7 +35,7 @@
  #include "net/base/network_change_notifier_linux.h"
@@ -9,15 +9,6 @@
  #include "net/base/network_change_notifier_posix.h"
  #elif defined(OS_FUCHSIA)
  #include "net/base/network_change_notifier_fuchsia.h"
-@@ -229,7 +229,7 @@ std::unique_ptr<NetworkChangeNotifier> NetworkChangeNo
-   // service in a separate process.
-   return std::make_unique<NetworkChangeNotifierPosix>(initial_type,
-                                                       initial_subtype);
--#elif defined(OS_CHROMEOS)
-+#elif defined(OS_CHROMEOS) || defined(OS_BSD)
-   return std::make_unique<NetworkChangeNotifierPosix>(initial_type,
-                                                       initial_subtype);
- #elif defined(OS_LINUX)
 @@ -241,7 +241,6 @@ std::unique_ptr<NetworkChangeNotifier> NetworkChangeNo
    return std::make_unique<NetworkChangeNotifierFuchsia>(
        0 /* required_features */);
