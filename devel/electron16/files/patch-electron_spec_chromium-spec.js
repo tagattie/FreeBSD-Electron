0836dd36ee7c79783c037a6d153204db8a792dbc
Hiroki Tagato <tagattie@gmail.com>
2021-12-22 10:12:42 +0900

    devel/electron16: update to 16.0.5
    Changelog: https://github.com/electron/electron/releases/tag/v16.0.5

--- devel/electron16/files/patch-electron_spec_chromium-spec.js
+++ devel/electron16/files/patch-electron_spec_chromium-spec.js
@@ -1,6 +1,6 @@
---- electron/spec/chromium-spec.js.orig	2021-04-20 23:32:33 UTC
+--- electron/spec/chromium-spec.js.orig	2021-12-16 22:06:44 UTC
 +++ electron/spec/chromium-spec.js
-@@ -234,7 +234,7 @@ describe('chromium feature', () => {
+@@ -224,7 +224,7 @@ describe('chromium feature', () => {
      });
  
      it('can be get as context in canvas', () => {
