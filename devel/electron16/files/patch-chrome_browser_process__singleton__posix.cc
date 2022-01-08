ba5422870f0b0b9ef966718135119552daa661ed
Hiroki Tagato <tagattie@gmail.com>
2022-01-08 13:44:54 +0900

    devel/electron16: update to 16.0.6
    Changelog: https://github.com/electron/electron/releases/tag/v16.0.6

--- devel/electron16/files/patch-chrome_browser_process__singleton__posix.cc
+++ devel/electron16/files/patch-chrome_browser_process__singleton__posix.cc
@@ -1,6 +1,6 @@
---- chrome/browser/process_singleton_posix.cc.orig	2021-12-18 06:49:11 UTC
+--- chrome/browser/process_singleton_posix.cc.orig	2022-01-07 07:48:36 UTC
 +++ chrome/browser/process_singleton_posix.cc
-@@ -97,13 +97,13 @@
+@@ -99,13 +99,13 @@
  #include "ui/base/l10n/l10n_util.h"
  
  #if 0
@@ -16,7 +16,7 @@
  #include "ui/views/linux_ui/linux_ui.h"
  #endif
  
-@@ -303,7 +303,7 @@ bool DisplayProfileInUseError(const base::FilePath& lo
+@@ -362,7 +362,7 @@ bool DisplayProfileInUseError(const base::FilePath& lo
    if (g_disable_prompt)
      return g_user_opted_unlock_in_use_profile;
  
@@ -25,7 +25,7 @@
    std::u16string relaunch_button_text =
        l10n_util::GetStringUTF16(IDS_PROFILE_IN_USE_LINUX_RELAUNCH);
    return ShowProcessSingletonDialog(error, relaunch_button_text);
-@@ -938,7 +938,7 @@ ProcessSingleton::NotifyResult ProcessSingleton::Notif
+@@ -997,7 +997,7 @@ ProcessSingleton::NotifyResult ProcessSingleton::Notif
      return PROCESS_NONE;
    } else if (strncmp(buf, kACKToken, base::size(kACKToken) - 1) == 0) {
  #if defined(TOOLKIT_VIEWS) && \
--- devel/electron16/files/patch-chrome_browser_web__applications_web__app__install__task.cc
+++ devel/electron16/files/patch-chrome_browser_web__applications_web__app__install__task.cc
@@ -1,6 +1,6 @@
---- chrome/browser/web_applications/web_app_install_task.cc.orig	2021-10-08 06:25:42 UTC
+--- chrome/browser/web_applications/web_app_install_task.cc.orig	2021-12-14 11:45:00 UTC
 +++ chrome/browser/web_applications/web_app_install_task.cc
-@@ -853,7 +853,7 @@ void WebAppInstallTask::OnInstallFinalizedCreateShortc
+@@ -857,7 +857,7 @@ void WebAppInstallTask::OnInstallFinalizedCreateShortc
    options.os_hooks[OsHookType::kProtocolHandlers] = true;
    options.os_hooks[OsHookType::kUninstallationViaOsSettings] = true;
  #if defined(OS_WIN) || defined(OS_MAC) || \
