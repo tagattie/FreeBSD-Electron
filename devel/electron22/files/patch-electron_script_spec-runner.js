214c45620208c43e9050ef26375603bb5a176f93
Hiroki Tagato <tagattie@gmail.com>
2023-01-30 15:44:51 +0900

    devel/electron22: update to 22.1.0
    Changelog: https://github.com/electron/electron/releases/tag/v22.1.0

--- devel/electron22/files/patch-electron_script_spec-runner.js
+++ devel/electron22/files/patch-electron_script_spec-runner.js
@@ -1,8 +1,8 @@
---- electron/script/spec-runner.js.orig	2023-01-18 11:12:36 UTC
+--- electron/script/spec-runner.js.orig	2023-01-24 16:58:16 UTC
 +++ electron/script/spec-runner.js
-@@ -124,7 +124,7 @@ async function runElectronTests () {
- async function runTestUsingElectron (specDir, testName) {
-   let exe = path.resolve(BASE, utils.getElectronExec());
+@@ -148,7 +148,7 @@ async function runTestUsingElectron (specDir, testName
+     exe = path.resolve(BASE, utils.getElectronExec());
+   }
    const runnerArgs = [`electron/${specDir}`, ...unknownArgs.slice(2)];
 -  if (process.platform === 'linux') {
 +  if (process.platform === 'linux' || process.platform === 'freebsd') {
