51ac06a8aca24efcafcc06b6362ffc045f46fec1
Hiroki Tagato <tagattie@gmail.com>
2023-02-15 23:08:59 +0900

    devel/electron23: add experimental port of electron 23
    Chromium patches obtained from:
    FreeBSD ports tree at 230cf3bad636e34e916bcba192aa5934d8fc5211
    
    Changelog: https://www.electronjs.org/blog/electron-23-0

---
+++ devel/electron23/files/patch-ui_color_color__id.h
@@ -0,0 +1,11 @@
+--- ui/color/color_id.h.orig	2023-02-15 13:08:45 UTC
++++ ui/color/color_id.h
+@@ -376,7 +376,7 @@
+   E_CPONLY(kColorNativeColor6) \
+   E_CPONLY(kColorNativeBaseColor) \
+   E_CPONLY(kColorNativeSecondaryColor)
+-#elif BUILDFLAG(IS_LINUX)
++#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ #define PLATFORM_SPECIFIC_COLOR_IDS \
+   E_CPONLY(kColorNativeButtonBorder)\
+   E_CPONLY(kColorNativeHeaderButtonBorderActive) \
