d818581553fd2bc7f2d1ee4e92c6f52a22b16c20
Hiroki Tagato <tagattie@gmail.com>
2023-05-24 17:52:13 +0900

    security/bitwarden-desktop: update to 2023.4.0
    Changelog: https://github.com/bitwarden/clients/releases/tag/desktop-v2023.4.0

--- security/bitwarden-desktop/files/patch-apps_desktop_desktop__native_src_biometric_mod.rs
+++ security/bitwarden-desktop/files/patch-apps_desktop_desktop__native_src_biometric_mod.rs
@@ -1,6 +1,8 @@
---- apps/desktop/desktop_native/src/biometric/mod.rs.orig	2023-02-09 07:28:13 UTC
+--- apps/desktop/desktop_native/src/biometric/mod.rs.orig	2023-04-26 11:30:57 UTC
 +++ apps/desktop/desktop_native/src/biometric/mod.rs
-@@ -1,4 +1,4 @@
+@@ -1,6 +1,6 @@
+ use anyhow::Result;
+ 
 -#[cfg_attr(target_os = "linux", path = "unix.rs")]
 +#[cfg_attr(any(target_os = "linux", target_os = "freebsd"), path = "unix.rs")]
  #[cfg_attr(target_os = "windows", path = "windows.rs")]
--- security/bitwarden-desktop/files/patch-apps_desktop_src_app_accounts_settings.component.ts
+++ security/bitwarden-desktop/files/patch-apps_desktop_src_app_accounts_settings.component.ts
@@ -1,8 +1,8 @@
---- apps/desktop/src/app/accounts/settings.component.ts.orig	2023-03-22 14:48:32 UTC
+--- apps/desktop/src/app/accounts/settings.component.ts.orig	2023-04-26 11:30:57 UTC
 +++ apps/desktop/src/app/accounts/settings.component.ts
-@@ -418,7 +418,7 @@ export class SettingsComponent implements OnInit {
+@@ -546,7 +546,7 @@ export class SettingsComponent implements OnInit {
  
-       this.enableBrowserIntegration = false;
+       this.form.controls.enableBrowserIntegration.setValue(false);
        return;
 -    } else if (process.platform == "linux") {
 +    } else if (process.platform == "linux" || process.platform === "freebsd") {
