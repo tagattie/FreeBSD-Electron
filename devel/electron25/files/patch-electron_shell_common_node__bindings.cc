4e711f1d195580cc3c90fc2cf98a4c995c8313cb
Hiroki Tagato <tagattie@gmail.com>
2023-06-13 17:19:15 +0900

    devel/electron25: update to 25.1.0
    Changelog: https://github.com/electron/electron/releases/tag/v25.1.0

--- devel/electron25/files/patch-electron_shell_common_node__bindings.cc
+++ devel/electron25/files/patch-electron_shell_common_node__bindings.cc
@@ -1,6 +1,6 @@
---- electron/shell/common/node_bindings.cc.orig	2023-05-31 14:28:29 UTC
+--- electron/shell/common/node_bindings.cc.orig	2023-06-07 10:09:25 UTC
 +++ electron/shell/common/node_bindings.cc
-@@ -40,7 +40,7 @@
+@@ -39,7 +39,7 @@
  #include "third_party/blink/renderer/bindings/core/v8/v8_initializer.h"  // nogncheck
  #include "third_party/electron_node/src/debug_utils.h"
  
@@ -9,7 +9,7 @@
  #include "shell/common/crash_keys.h"
  #endif
  
-@@ -157,7 +157,7 @@ bool g_is_initialized = false;
+@@ -156,7 +156,7 @@ bool g_is_initialized = false;
  void V8FatalErrorCallback(const char* location, const char* message) {
    LOG(ERROR) << "Fatal error in V8: " << location << " " << message;
  
@@ -18,7 +18,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-fatal.message", message);
    electron::crash_keys::SetCrashKey("electron.v8-fatal.location", location);
  #endif
-@@ -435,7 +435,7 @@ void NodeBindings::Initialize(v8::Local<v8::Context> c
+@@ -431,7 +431,7 @@ void NodeBindings::Initialize(v8::Local<v8::Context> c
    TRACE_EVENT0("electron", "NodeBindings::Initialize");
    // Open node's error reporting system for browser process.
  
