89c12c015ad1638ac196e4cf7ea6d04343d0dd47
Hiroki Tagato <tagattie@gmail.com>
2023-06-13 20:59:30 +0900

    security/bitwarden-desktop: update to 2023.5.0
    Changelog: https://github.com/bitwarden/clients/releases/tag/desktop-v2023.5.0

--- security/bitwarden-desktop/files/patch-apps_desktop_src_app_accounts_settings.component.ts
+++ security/bitwarden-desktop/files/patch-apps_desktop_src_app_accounts_settings.component.ts
@@ -1,11 +1,11 @@
---- apps/desktop/src/app/accounts/settings.component.ts.orig	2023-04-26 11:30:57 UTC
+--- apps/desktop/src/app/accounts/settings.component.ts.orig	2023-05-31 00:42:10 UTC
 +++ apps/desktop/src/app/accounts/settings.component.ts
-@@ -546,7 +546,7 @@ export class SettingsComponent implements OnInit {
+@@ -543,7 +543,7 @@ export class SettingsComponent implements OnInit {
  
        this.form.controls.enableBrowserIntegration.setValue(false);
        return;
 -    } else if (process.platform == "linux") {
-+    } else if (process.platform == "linux" || process.platform === "freebsd") {
-       await this.platformUtilsService.showDialog(
-         this.i18nService.t("browserIntegrationLinuxDesc"),
-         this.i18nService.t("browserIntegrationUnsupportedTitle"),
++    } else if (process.platform == "linux" || process.platform == "freebsd") {
+       await this.dialogService.openSimpleDialog({
+         title: { key: "browserIntegrationUnsupportedTitle" },
+         content: { key: "browserIntegrationLinuxDesc" },
