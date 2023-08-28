ba43813ea8fc375483112d3eb2a9db421e831b5e
Hiroki Tagato <tagattie@gmail.com>
2023-08-28 18:03:45 +0900

    security/bitwarden-desktop: update to 2023.8.2
    Changelog: https://github.com/bitwarden/clients/releases/tag/desktop-v2023.8.2

--- security/bitwarden-desktop/files/patch-apps_desktop_src_app_accounts_settings.component.ts
+++ security/bitwarden-desktop/files/patch-apps_desktop_src_app_accounts_settings.component.ts
@@ -1,6 +1,6 @@
---- apps/desktop/src/app/accounts/settings.component.ts.orig	2023-05-31 00:42:10 UTC
+--- apps/desktop/src/app/accounts/settings.component.ts.orig	2023-08-24 18:31:12 UTC
 +++ apps/desktop/src/app/accounts/settings.component.ts
-@@ -543,7 +543,7 @@ export class SettingsComponent implements OnInit {
+@@ -600,7 +600,7 @@ export class SettingsComponent implements OnInit {
  
        this.form.controls.enableBrowserIntegration.setValue(false);
        return;
--- security/bitwarden-desktop/files/patch-apps_desktop_src_main_window.main.ts
+++ security/bitwarden-desktop/files/patch-apps_desktop_src_main_window.main.ts
@@ -1,6 +1,6 @@
---- apps/desktop/src/main/window.main.ts.orig	2023-03-22 14:48:32 UTC
+--- apps/desktop/src/main/window.main.ts.orig	2023-08-24 18:31:12 UTC
 +++ apps/desktop/src/main/window.main.ts
-@@ -48,7 +48,7 @@ export class WindowMain {
+@@ -66,7 +66,7 @@ export class WindowMain {
                  }
                  this.win.focus();
                }
@@ -9,7 +9,7 @@
                  if (this.argvCallback != null) {
                    this.argvCallback(argv);
                  }
-@@ -117,7 +117,7 @@ export class WindowMain {
+@@ -137,7 +137,7 @@ export class WindowMain {
        x: this.windowStates[mainWindowSizeKey].x,
        y: this.windowStates[mainWindowSizeKey].y,
        title: app.name,
@@ -17,4 +17,4 @@
 +      icon: (process.platform === "linux" || process.platform === "freebsd") ? path.join(__dirname, "/images/icon.png") : undefined,
        titleBarStyle: process.platform === "darwin" ? "hiddenInset" : undefined,
        show: false,
-       backgroundColor: "#fff",
+       backgroundColor: await this.getBackgroundColor(),
