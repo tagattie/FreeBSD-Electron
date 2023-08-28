ae542588c45320e1f67bf0da811b7adc4ece0dc1
Hiroki Tagato <tagattie@gmail.com>
2023-08-28 20:49:53 +0900

    devel/github-desktop: update to 3.2.9
    Changelog:
    - https://github.com/shiftkey/desktop/releases/tag/release-3.2.8-linux1
    - https://github.com/shiftkey/desktop/releases/tag/release-3.2.9-linux1

--- devel/github-desktop/files/patch-app_test_globals.ts
+++ devel/github-desktop/files/patch-app_test_globals.ts
@@ -1,11 +1,11 @@
---- app/test/globals.ts.orig	2023-03-14 01:36:07 UTC
+--- app/test/globals.ts.orig	2023-08-26 16:06:35 UTC
 +++ app/test/globals.ts
-@@ -11,7 +11,7 @@ import 'airbnb-browser-shims/browser-only'
+@@ -12,7 +12,7 @@ import { join } from 'path'
  const g: any = global
  g['__WIN32__'] = process.platform === 'win32'
  g['__DARWIN__'] = process.platform === 'darwin'
 -g['__LINUX__'] = process.platform === 'linux'
 +g['__LINUX__'] = (process.platform === 'linux' || process.platform === 'freebsd')
+ g['__APP_VERSION__'] = require(join(__dirname, '../package.json')).version
  g['__DEV__'] = 'false'
  g['__RELEASE_CHANNEL__'] = 'development'
- g['__UPDATES_URL__'] = ''
--- devel/github-desktop/files/patch-script_build.ts
+++ devel/github-desktop/files/patch-script_build.ts
@@ -1,6 +1,6 @@
---- script/build.ts.orig	2023-08-16 01:24:56 UTC
+--- script/build.ts.orig	2023-08-26 16:06:35 UTC
 +++ script/build.ts
-@@ -145,6 +145,9 @@ function packageApp() {
+@@ -125,6 +125,9 @@ function packageApp() {
      if (platform === 'win32' || platform === 'darwin' || platform === 'linux') {
        return platform
      }
@@ -10,7 +10,7 @@
      throw new Error(
        `Unable to convert to platform for electron-packager: '${process.platform}`
      )
-@@ -185,7 +188,7 @@ function packageApp() {
+@@ -165,7 +168,7 @@ function packageApp() {
    // this setting only works for macOS and Windows, so let's clear it now to ensure
    // the app is working as expected
    const icon =
