887c1b8a04ee63392312b3129bc8c2bd1c14e79f
Hiroki Tagato <tagattie@gmail.com>
2023-10-26 18:23:32 +0900

    devel/electron27: add experimental port of electron 27
    Chromium patches obtained from:
    FreeBSD ports tree at 631589e7e2c357e1ca88a8ef7b0112b1b7d376cc
    
    Changelog: https://www.electronjs.org/blog/electron-27-0

---
+++ devel/electron27/files/patch-components_password__manager_core_common_password__manager__features.cc
@@ -0,0 +1,20 @@
+--- components/password_manager/core/common/password_manager_features.cc.orig	2023-10-19 19:58:18 UTC
++++ components/password_manager/core/common/password_manager_features.cc
+@@ -18,7 +18,7 @@ BASE_FEATURE(kEnableOverwritingPlaceholderUsernames,
+              "EnableOverwritingPlaceholderUsernames",
+              base::FEATURE_DISABLED_BY_DEFAULT);
+ 
+-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
++#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ // When enabled, initial sync will be forced during startup if the password
+ // store has encryption service failures.
+ BASE_FEATURE(kForceInitialSyncWhenDecryptionFails,
+@@ -99,7 +99,7 @@ BASE_FEATURE(kRecoverFromNeverSaveAndroid,
+              "RecoverFromNeverSaveAndroid_LAUNCHED",
+              base::FEATURE_ENABLED_BY_DEFAULT);
+ 
+-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
++#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ // Displays at least the decryptable and never saved logins in the password
+ // manager
+ BASE_FEATURE(kSkipUndecryptablePasswords,
---
+++ devel/electron27/files/patch-components_password__manager_core_common_password__manager__features.h
@@ -0,0 +1,20 @@
+--- components/password_manager/core/common/password_manager_features.h.orig	2023-10-19 19:58:18 UTC
++++ components/password_manager/core/common/password_manager_features.h
+@@ -24,7 +24,7 @@ namespace password_manager::features {
+ // alongside the definition of their values in the .cc file.
+ BASE_DECLARE_FEATURE(kEnableOverwritingPlaceholderUsernames);
+ 
+-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
++#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ BASE_DECLARE_FEATURE(kForceInitialSyncWhenDecryptionFails);
+ #endif
+ BASE_DECLARE_FEATURE(kForgotPasswordFormSupport);
+@@ -43,7 +43,7 @@ BASE_DECLARE_FEATURE(kPasswordGenerationExperiment);
+ #endif
+ BASE_DECLARE_FEATURE(kPasswordsImportM2);
+ BASE_DECLARE_FEATURE(kRecoverFromNeverSaveAndroid);
+-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
++#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ BASE_DECLARE_FEATURE(kSkipUndecryptablePasswords);
+ #endif
+ #if BUILDFLAG(IS_ANDROID)
