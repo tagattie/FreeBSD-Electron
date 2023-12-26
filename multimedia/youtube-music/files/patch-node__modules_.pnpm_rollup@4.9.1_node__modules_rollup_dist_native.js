c6ed0240a05f67f1bb5f9ab91fdd4e8656ab6768
Hiroki Tagato <tagattie@gmail.com>
2023-12-26 18:20:11 +0900

    multimedia/youtube-music: update to 3.1.1
    Changelog: https://github.com/th-ch/youtube-music/blob/master/changelog.md#v311

---
+++ multimedia/youtube-music/files/patch-node__modules_.pnpm_rollup@4.9.1_node__modules_rollup_dist_native.js
@@ -0,0 +1,13 @@
+--- node_modules/.pnpm/rollup@4.9.1/node_modules/rollup/dist/native.js.orig	2023-12-26 09:00:23 UTC
++++ node_modules/.pnpm/rollup@4.9.1/node_modules/rollup/dist/native.js
+@@ -19,6 +19,10 @@ const bindingsByPlatformAndArch = {
+ 		riscv64: { base: 'linux-riscv64-gnu', musl: null },
+ 		x64: { base: 'linux-x64-gnu', musl: 'linux-x64-musl' }
+ 	},
++	freebsd: {
++		arm64: { base: 'freebsd-arm64' },
++		x64: { base: 'freebsd-x64' }
++	},
+ 	win32: {
+ 		arm64: { base: 'win32-arm64-msvc' },
+ 		ia32: { base: 'win32-ia32-msvc' },
