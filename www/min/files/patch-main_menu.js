45bba6025947b6ca565d7d1f79ae359df2cb58bf
Hiroki Tagato <tagattie@gmail.com>
2024-03-02 14:44:47 +0900

    www/min: update to 1.31.2
    Changelog:
    - https://github.com/minbrowser/min/releases/tag/v1.31.0
    - https://github.com/minbrowser/min/releases/tag/v1.31.1
    - https://github.com/minbrowser/min/releases/tag/v1.31.2

--- www/min/files/patch-main_menu.js
+++ www/min/files/patch-main_menu.js
@@ -1,6 +1,6 @@
---- main/menu.js.orig	2023-11-25 18:12:52 UTC
+--- main/menu.js.orig	2024-02-22 21:51:15 UTC
 +++ main/menu.js
-@@ -168,8 +168,8 @@ function buildAppMenu (options = {}) {
+@@ -170,8 +170,8 @@ function buildAppMenu (options = {}) {
              sendIPCToWindow(window, 'print')
            }
          },
