9384ed49a54f7530d24afb62825170ab2978de25
Hiroki Tagato <tagattie@gmail.com>
2024-03-09 21:10:11 +0900

    deskutils/joplin-desktop: update to 2.14.19

--- deskutils/joplin-desktop/files/patch-packages_app-desktop_ElectronAppWrapper.ts
+++ deskutils/joplin-desktop/files/patch-packages_app-desktop_ElectronAppWrapper.ts
@@ -1,6 +1,6 @@
---- packages/app-desktop/ElectronAppWrapper.ts.orig	2023-12-24 11:26:28 UTC
+--- packages/app-desktop/ElectronAppWrapper.ts.orig	2024-02-26 19:28:48 UTC
 +++ packages/app-desktop/ElectronAppWrapper.ts
-@@ -148,7 +148,7 @@ export default class ElectronAppWrapper {
+@@ -151,7 +151,7 @@ export default class ElectronAppWrapper {
  
  		// Linux icon workaround for bug https://github.com/electron-userland/electron-builder/issues/2098
  		// Fix: https://github.com/electron-userland/electron-builder/issues/2269
--- deskutils/joplin-desktop/files/patch-packages_app-desktop_bridge.ts
+++ deskutils/joplin-desktop/files/patch-packages_app-desktop_bridge.ts
@@ -1,6 +1,6 @@
---- packages/app-desktop/bridge.ts.orig	2023-12-24 11:26:28 UTC
+--- packages/app-desktop/bridge.ts.orig	2024-03-07 10:01:48 UTC
 +++ packages/app-desktop/bridge.ts
-@@ -277,7 +277,7 @@ export class Bridge {
+@@ -364,7 +364,7 @@ export class Bridge {
  				execPath: process.env.PORTABLE_EXECUTABLE_FILE,
  			};
  			app.relaunch(options);
