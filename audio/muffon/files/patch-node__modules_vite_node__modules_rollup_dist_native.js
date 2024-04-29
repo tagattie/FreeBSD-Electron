5805cef7bec0bf752880041a3ffcb116d5413e29
Hiroki Tagato <tagattie@gmail.com>
2024-04-30 05:46:02 +0900

    audio/muffon: update to 2.0.3
    Changelog: https://github.com/staniel359/muffon/releases/tag/v2.0.3

--- audio/muffon/files/patch-node__modules_vite_node__modules_rollup_dist_native.js
+++ audio/muffon/files/patch-node__modules_vite_node__modules_rollup_dist_native.js
@@ -1,13 +1,13 @@
---- node_modules/vite/node_modules/rollup/dist/native.js.orig	2024-03-23 06:06:16 UTC
+--- node_modules/vite/node_modules/rollup/dist/native.js.orig	2024-04-29 20:32:37 UTC
 +++ node_modules/vite/node_modules/rollup/dist/native.js
-@@ -19,6 +19,10 @@ const bindingsByPlatformAndArch = {
- 		riscv64: { base: 'linux-riscv64-gnu', musl: null },
- 		x64: { base: 'linux-x64-gnu', musl: 'linux-x64-musl' }
+@@ -13,6 +13,10 @@ const bindingsByPlatformAndArch = {
+ 		arm64: { base: 'darwin-arm64' },
+ 		x64: { base: 'darwin-x64' }
  	},
 +	freebsd: {
 +		arm64: { base: 'freebsd-arm64' },
 +		x64: { base: 'freebsd-x64' }
 +	},
- 	win32: {
- 		arm64: { base: 'win32-arm64-msvc' },
- 		ia32: { base: 'win32-ia32-msvc' },
+ 	linux: {
+ 		arm: { base: 'linux-arm-gnueabihf', musl: 'linux-arm-musleabihf' },
+ 		arm64: { base: 'linux-arm64-gnu', musl: 'linux-arm64-musl' },
