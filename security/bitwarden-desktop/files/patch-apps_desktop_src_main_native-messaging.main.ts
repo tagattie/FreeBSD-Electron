ae4a4ca8d9b0ae76830fde452e091b420e28c816
Hiroki Tagato <tagattie@gmail.com>
2024-06-05 17:50:00 +0900

    security/bitwarden-desktop: update to 2024.5.0
    Changelog: https://github.com/bitwarden/clients/releases/tag/desktop-v2024.5.0

--- security/bitwarden-desktop/files/patch-apps_desktop_src_main_native-messaging.main.ts
+++ security/bitwarden-desktop/files/patch-apps_desktop_src_main_native-messaging.main.ts
@@ -1,6 +1,6 @@
---- apps/desktop/src/main/native-messaging.main.ts.orig	2024-05-06 13:04:55 UTC
+--- apps/desktop/src/main/native-messaging.main.ts.orig	2024-05-20 20:31:10 UTC
 +++ apps/desktop/src/main/native-messaging.main.ts
-@@ -188,7 +188,7 @@ export class NativeMessagingMain {
+@@ -190,7 +190,7 @@ export class NativeMessagingMain {
          }
          break;
        }
@@ -9,7 +9,7 @@
          if (existsSync(`${this.homedir()}/.mozilla/`)) {
            await this.writeManifest(
              `${this.homedir()}/.mozilla/native-messaging-hosts/com.8bit.bitwarden.json`,
-@@ -260,7 +260,7 @@ export class NativeMessagingMain {
+@@ -262,7 +262,7 @@ export class NativeMessagingMain {
          }
          break;
        }
