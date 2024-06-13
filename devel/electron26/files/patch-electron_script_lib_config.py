003a5ef7fb6651541d613817c06798f3fcc5f3f7
Hiroki Tagato <tagattie@gmail.com>
2024-06-13 12:23:09 +0900

    devel/electron{25,26,27,28,29,30}: fix build on FreeBSD 15-current
    While here, remove support for obsolete FreeBSD 12.

--- devel/electron26/files/patch-electron_script_lib_config.py
+++ devel/electron26/files/patch-electron_script_lib_config.py
@@ -1,12 +1,12 @@
---- electron/script/lib/config.py.orig	2021-11-08 18:41:28 UTC
+--- electron/script/lib/config.py.orig	2024-02-14 21:53:19 UTC
 +++ electron/script/lib/config.py
 @@ -8,6 +8,9 @@ PLATFORM = {
    'cygwin': 'win32',
    'msys': 'win32',
    'darwin': 'darwin',
-+  'freebsd12': 'freebsd',
 +  'freebsd13': 'freebsd',
 +  'freebsd14': 'freebsd',
++  'freebsd15': 'freebsd',
    'linux': 'linux',
    'linux2': 'linux',
    'win32': 'win32',
