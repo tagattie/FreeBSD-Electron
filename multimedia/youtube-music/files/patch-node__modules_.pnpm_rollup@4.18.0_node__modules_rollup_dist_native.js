acfff14cd53544b100bf62fd63aab1ac4c6401a4
Hiroki Tagato <tagattie@gmail.com>
2024-06-27 05:07:34 +0900

    multimedia/youtube-music: update to 3.3.12
    Changelog: https://github.com/th-ch/youtube-music/blob/master/changelog.md#v3312

--- multimedia/youtube-music/files/patch-node__modules_.pnpm_rollup@4.18.0_node__modules_rollup_dist_native.js
+++ multimedia/youtube-music/files/patch-node__modules_.pnpm_rollup@4.18.0_node__modules_rollup_dist_native.js
@@ -1,5 +1,5 @@
---- node_modules/.pnpm/rollup@4.14.2/node_modules/rollup/dist/native.js.orig	2024-04-29 11:35:58 UTC
-+++ node_modules/.pnpm/rollup@4.14.2/node_modules/rollup/dist/native.js
+--- node_modules/.pnpm/rollup@4.18.0/node_modules/rollup/dist/native.js.orig	2024-06-26 19:58:07 UTC
++++ node_modules/.pnpm/rollup@4.18.0/node_modules/rollup/dist/native.js
 @@ -13,6 +13,10 @@ const bindingsByPlatformAndArch = {
  		arm64: { base: 'darwin-arm64' },
  		x64: { base: 'darwin-x64' }
@@ -9,5 +9,5 @@
 +		x64: { base: 'freebsd-x64' }
 +	},
  	linux: {
- 		arm: { base: 'linux-arm-gnueabihf', musl: null },
+ 		arm: { base: 'linux-arm-gnueabihf', musl: 'linux-arm-musleabihf' },
  		arm64: { base: 'linux-arm64-gnu', musl: 'linux-arm64-musl' },
