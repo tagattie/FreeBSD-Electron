1b2a09bca03ffe3a5995b41341466c52e6dbdda8
Hiroki Tagato <tagattie@gmail.com>
2024-07-20 12:28:47 +0900

    net-im/element-desktop: update to 1.11.71
    Changelog:
    - https://github.com/element-hq/element-desktop/releases/tag/v1.11.66
    - https://github.com/element-hq/element-desktop/releases/tag/v1.11.67
    - https://github.com/element-hq/element-desktop/releases/tag/v1.11.68
    - https://github.com/element-hq/element-desktop/releases/tag/v1.11.69
    - https://github.com/element-hq/element-desktop/releases/tag/v1.11.70
    - https://github.com/element-hq/element-desktop/releases/tag/v1.11.71

--- net-im/element-desktop/files/patch-electron-builder.ts
+++ net-im/element-desktop/files/patch-electron-builder.ts
@@ -1,6 +1,6 @@
---- electron-builder.ts.orig	2024-04-23 13:14:00 UTC
+--- electron-builder.ts.orig	2024-07-16 12:55:06 UTC
 +++ electron-builder.ts
-@@ -56,7 +56,7 @@ const config: Writable<Configuration> = {
+@@ -54,7 +54,7 @@ const config: Writable<Configuration> = {
  const config: Writable<Configuration> = {
      appId: "im.riot.app",
      asarUnpack: "**/*.node",
@@ -9,7 +9,7 @@
          if (context.electronPlatformName !== "darwin" || context.arch === Arch.universal) {
              // Burn in electron fuses for proactive security hardening.
              // On macOS, we only do this for the universal package, as the constituent arm64 and amd64 packages are embedded within.
-@@ -92,7 +92,7 @@ const config: Writable<Configuration> = {
+@@ -90,7 +90,7 @@ const config: Writable<Configuration> = {
                  [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: false,
              });
          }
