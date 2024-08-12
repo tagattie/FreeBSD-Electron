e892dc55141b78fd064a1272c236db807b75fe1c
Hiroki Tagato <tagattie@gmail.com>
2024-08-12 17:38:53 +0900

    multimedia/losslesscut: update to 3.62.0
    Changelog: https://github.com/mifi/lossless-cut/releases/tag/v3.62.0

---
+++ multimedia/losslesscut/files/patch-node__modules_rollup_dist_native.js
@@ -0,0 +1,13 @@
+--- node_modules/rollup/dist/native.js.orig	2024-08-12 08:12:38 UTC
++++ node_modules/rollup/dist/native.js
+@@ -13,6 +13,10 @@ const bindingsByPlatformAndArch = {
+ 		arm64: { base: 'darwin-arm64' },
+ 		x64: { base: 'darwin-x64' }
+ 	},
++	freebsd: {
++		arm64: { base: 'freebsd-arm64' },
++		x64: { base: 'freebsd-x64' }
++	},
+ 	linux: {
+ 		arm: { base: 'linux-arm-gnueabihf', musl: 'linux-arm-musleabihf' },
+ 		arm64: { base: 'linux-arm64-gnu', musl: 'linux-arm64-musl' },
