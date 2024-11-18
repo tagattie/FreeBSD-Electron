e6ef77c5b922a8d97c92a84391ee050790552124
Hiroki Tagato <tagattie@gmail.com>
2024-11-19 05:21:04 +0900

    deskutils/ueli: update to 9.9.1
    Changelog:
    - https://github.com/oliverschwendener/ueli/releases/tag/v9.8.0
    - https://github.com/oliverschwendener/ueli/releases/tag/v9.9.0
    - https://github.com/oliverschwendener/ueli/releases/tag/v9.9.1

--- deskutils/ueli/files/patch-src_main_Core_OperatingSystem_getOperatingSystemFromPlatform.ts
+++ deskutils/ueli/files/patch-src_main_Core_OperatingSystem_getOperatingSystemFromPlatform.ts
@@ -1,11 +1,6 @@
---- src/main/Core/OperatingSystem/getOperatingSystemFromPlatform.ts.orig	2024-07-23 21:51:24 UTC
+--- src/main/Core/OperatingSystem/getOperatingSystemFromPlatform.ts.orig	2024-11-11 07:16:12 UTC
 +++ src/main/Core/OperatingSystem/getOperatingSystemFromPlatform.ts
-@@ -1,10 +1,11 @@ export const getOperatingSystemFromPlatform = (platfor
- import type { OperatingSystem } from "@common/Core";
- 
- export const getOperatingSystemFromPlatform = (platform: string): OperatingSystem => {
--    const operatingSystemMap: Record<"darwin" | "win32" | "linux", OperatingSystem> = {
-+    const operatingSystemMap: Record<"darwin" | "win32" | "linux" | "freebsd", OperatingSystem> = {
+@@ -5,6 +5,7 @@ export const getOperatingSystemFromPlatform = (platfor
          darwin: "macOS",
          win32: "Windows",
          linux: "Linux",
