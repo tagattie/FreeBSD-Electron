10f864535fbedafb9905203601506554fb2ed01a
Hiroki Tagato <tagattie@gmail.com>
2024-11-19 05:17:07 +0900

    electron.mk: remove npm_config_build_from_source from MAKE_ENV
    Recent electron-builder throws an error when trying to cross-compile
    native node modules (e.g. trying electron-builder install-app-deps
    --platform linux on FreeBSD platform). So remove forcing build from
    source here.
    
    npm_config_build_from_source should be specified per port basis if
    needed.

--- Mk/Uses/electron.mk
+++ Mk/Uses/electron.mk
@@ -695,7 +695,6 @@ MAKE_ENV+=	PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1	# don't download browser for playw
 MAKE_ENV+=	PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1	# don't download chromium for puppeteer
 MAKE_ENV+=	SASS_FORCE_BUILD=true		# always rebuild native node-sass module
 MAKE_ENV+=	USE_SYSTEM_APP_BUILDER=true	# always use system app-builder for electron-builder
-MAKE_ENV+=	npm_config_build_from_source=true
 SUB_LIST+=	ELECTRON_VER_MAJOR=${ELECTRON_VER_MAJOR}
 
 .endif # _INCLUDE_USES_ELECTRON_MK
