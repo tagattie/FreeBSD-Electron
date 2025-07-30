6a846d3d89b34f9eaa6555569ccbe7831a28f929
Hiroki Tagato <tagattie@gmail.com>
2025-07-30 15:19:37 +0900

    net-im/element-desktop: Update to 1.11.107
    Changelog: https://github.com/element-hq/element-desktop/blob/v1.11.107/CHANGELOG.md

--- net-im/element-desktop/files/patch-src_electron-main.ts
+++ net-im/element-desktop/files/patch-src_electron-main.ts
@@ -1,6 +1,6 @@
---- src/electron-main.ts.orig	2025-06-03 15:13:27 UTC
+--- src/electron-main.ts.orig	2025-07-29 13:25:01 UTC
 +++ src/electron-main.ts
-@@ -304,7 +304,7 @@ if (!app.commandLine.hasSwitch("enable-features")) {
+@@ -305,7 +305,7 @@ if (!app.commandLine.hasSwitch("enable-features")) {
  }
  // Workaround bug in electron 36:https://github.com/electron/electron/issues/46538
  // Hopefully this will no longer be needed soon and can be removed
--- net-im/element-desktop/files/patch-src_ipc.ts
+++ net-im/element-desktop/files/patch-src_ipc.ts
@@ -1,6 +1,6 @@
---- src/ipc.ts.orig	2025-07-01 15:32:42 UTC
+--- src/ipc.ts.orig	2025-07-29 13:25:01 UTC
 +++ src/ipc.ts
-@@ -26,7 +26,7 @@ ipcMain.on("loudNotification", function (): void {
+@@ -14,7 +14,7 @@ ipcMain.on("loudNotification", function (): void {
  
  let focusHandlerAttached = false;
  ipcMain.on("loudNotification", function (): void {
