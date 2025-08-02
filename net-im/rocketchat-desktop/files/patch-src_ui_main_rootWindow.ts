64b18e15c8ad040a255f48d9461c3b5a6516ce1f
Hiroki Tagato <tagattie@gmail.com>
2025-08-03 07:09:32 +0900

    net-im/rocketchat-desktop: update to 4.8.0
    Changelog: https://github.com/RocketChat/Rocket.Chat.Electron/releases/tag/4.8.0

--- net-im/rocketchat-desktop/files/patch-src_ui_main_rootWindow.ts
+++ net-im/rocketchat-desktop/files/patch-src_ui_main_rootWindow.ts
@@ -1,6 +1,6 @@
---- src/ui/main/rootWindow.ts.orig	2025-05-12 16:50:31 UTC
+--- src/ui/main/rootWindow.ts.orig	2025-07-30 23:45:16 UTC
 +++ src/ui/main/rootWindow.ts
-@@ -268,7 +268,7 @@ export const setupRootWindow = (): void => {
+@@ -281,7 +281,7 @@ export const setupRootWindow = (): void => {
          rootWindow.setFullScreen(false);
        }
  
@@ -9,7 +9,7 @@
  
        const isTrayIconEnabled = select(
          ({ isTrayIconEnabled }) => isTrayIconEnabled ?? true
-@@ -297,7 +297,7 @@ export const setupRootWindow = (): void => {
+@@ -310,7 +310,7 @@ export const setupRootWindow = (): void => {
      });
    });
  
@@ -18,7 +18,7 @@
      const selectRootWindowIcon = createStructuredSelector({
        globalBadge: selectGlobalBadge,
        rootWindowIcon: ({ rootWindowIcon }: RootState) => rootWindowIcon,
-@@ -322,7 +322,7 @@ export const setupRootWindow = (): void => {
+@@ -335,7 +335,7 @@ export const setupRootWindow = (): void => {
          const icon = nativeImage.createEmpty();
          const { scaleFactor } = screen.getPrimaryDisplay();
  
