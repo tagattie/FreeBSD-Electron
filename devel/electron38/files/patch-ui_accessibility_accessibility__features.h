c9c4e9deaced918bcb81440d31f7569741a9c27c
Hiroki Tagato <tagattie@gmail.com>
2025-09-11 14:30:52 +0900

    devel/electron38: add experimental port of electron 38
    Chromium patches obtained from:
    FreeBSD ports tree at d7a8c7e76b85da4c340babd701e2e9088568daa9.
    
    Changelog: https://www.electronjs.org/blog/electron-38-0

---
+++ devel/electron38/files/patch-ui_accessibility_accessibility__features.h
@@ -0,0 +1,20 @@
+--- ui/accessibility/accessibility_features.h.orig	2025-08-26 20:49:50 UTC
++++ ui/accessibility/accessibility_features.h
+@@ -339,7 +339,7 @@ AX_BASE_EXPORT bool IsScreenAITestModeEnabled();
+ AX_BASE_EXPORT BASE_DECLARE_FEATURE(kScreenAITestMode);
+ AX_BASE_EXPORT bool IsScreenAITestModeEnabled();
+ 
+-#if BUILDFLAG(IS_LINUX)
++#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ // Enables advanced partition allocation checks in ScreenAI service.
+ // TODO(crbug.com/418199684): Remove when the bug is fixed.
+ AX_BASE_EXPORT BASE_DECLARE_FEATURE(
+@@ -366,7 +366,7 @@ AX_BASE_EXPORT bool IsBlockRootWindowAccessibleNameCha
+ AX_BASE_EXPORT bool IsBlockRootWindowAccessibleNameChangeEventEnabled();
+ #endif  // BUILDFLAG(IS_MAC)
+ 
+-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
++#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ // Use the v3 version of the wasm tts engine component.
+ AX_BASE_EXPORT BASE_DECLARE_FEATURE(kWasmTtsComponentUpdaterV3Enabled);
+ AX_BASE_EXPORT bool IsWasmTtsComponentUpdaterV3Enabled();
