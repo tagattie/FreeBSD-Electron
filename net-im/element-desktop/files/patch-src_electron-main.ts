97e639d7c1dd1d89baf7c0d24d3178b25fd69a40
Hiroki Tagato <tagattie@gmail.com>
2025-09-16 15:28:00 +0900

    net-im/element-desktop: Update to 1.11.111
    Changelog: https://github.com/element-hq/element-desktop/blob/v1.11.111/CHANGELOG.md

--- net-im/element-desktop/files/patch-src_electron-main.ts
+++ net-im/element-desktop/files/patch-src_electron-main.ts
@@ -1,6 +1,6 @@
---- src/electron-main.ts.orig	2025-07-29 13:25:01 UTC
+--- src/electron-main.ts.orig	2025-09-10 09:48:57 UTC
 +++ src/electron-main.ts
-@@ -305,7 +305,7 @@ if (!app.commandLine.hasSwitch("enable-features")) {
+@@ -277,7 +277,7 @@ if (!app.commandLine.hasSwitch("enable-features")) {
  }
  // Workaround bug in electron 36:https://github.com/electron/electron/issues/46538
  // Hopefully this will no longer be needed soon and can be removed
