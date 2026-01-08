1944a64ae546a517c2f12894b3b5a89cc345beae
Hiroki Tagato <tagattie@gmail.com>
2026-01-08 22:10:12 +0900

    deskutils/superproductivity: Update to 16.8.3
    Changelog: https://github.com/super-productivity/super-productivity/blob/v16.8.3/CHANGELOG.md

--- deskutils/superproductivity/files/patch-electron_preload.ts
+++ deskutils/superproductivity/files/patch-electron_preload.ts
@@ -1,4 +1,4 @@
---- electron/preload.ts.orig	2025-11-14 14:43:39 UTC
+--- electron/preload.ts.orig	2026-01-04 16:27:04 UTC
 +++ electron/preload.ts
 @@ -56,7 +56,7 @@ const ea: ElectronAPI = {
      webFrame.setZoomFactor(zoomFactor);
@@ -8,4 +8,4 @@
 +  isLinux: () => process.platform === 'linux' || process.platform === 'freebsd',
    isMacOS: () => process.platform === 'darwin',
    isSnap: () => process && process.env && !!process.env.SNAP,
- 
+   isFlatpak: () => process && process.env && !!process.env.FLATPAK_ID,
