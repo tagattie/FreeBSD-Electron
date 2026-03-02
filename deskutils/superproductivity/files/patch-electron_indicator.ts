b399a5276c7e67723c7e57331fbd71266430e249
Hiroki Tagato <tagattie@gmail.com>
2026-03-02 21:05:07 +0900

    deskutils/superproductivity: Update to 17.2.4
    Changelog: https://github.com/super-productivity/super-productivity/blob/v17.2.4/CHANGELOG.md

--- deskutils/superproductivity/files/patch-electron_indicator.ts
+++ deskutils/superproductivity/files/patch-electron_indicator.ts
@@ -1,11 +1,11 @@
---- electron/indicator.ts.orig	2026-02-06 19:11:32 UTC
+--- electron/indicator.ts.orig	2026-02-28 23:19:19 UTC
 +++ electron/indicator.ts
-@@ -14,7 +14,7 @@ const IS_MAC = process.platform === 'darwin';
- let DIR: string;
- let shouldUseDarkColors: boolean;
+@@ -41,7 +41,7 @@ const IS_MAC = process.platform === 'darwin';
+ let _lastFocusModeMode: string;
+ 
  const IS_MAC = process.platform === 'darwin';
 -const IS_LINUX = process.platform === 'linux';
 +const IS_LINUX = process.platform === 'linux' || process.platform === 'freebsd';
  const IS_WINDOWS = process.platform === 'win32';
  
- // Static GUID for Windows tray icon position persistence across updates.
+ // Stable GUIDs per Windows distribution type.
