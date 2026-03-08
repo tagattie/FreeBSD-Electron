eaa429a5d235b7b51953660b3af185bc14b542ab
Hiroki Tagato <tagattie@gmail.com>
2026-03-08 15:12:07 +0900

    security/bitwarden-desktop: Update to 2026.2.1
    Changelog: https://github.com/bitwarden/clients/releases/tag/desktop-v2026.2.1

--- security/bitwarden-desktop/files/patch-apps_desktop_webpack.base.js
+++ security/bitwarden-desktop/files/patch-apps_desktop_webpack.base.js
@@ -1,6 +1,6 @@
---- apps/desktop/webpack.base.js.orig	2026-02-02 09:34:46 UTC
+--- apps/desktop/webpack.base.js.orig	2026-03-02 17:39:37 UTC
 +++ apps/desktop/webpack.base.js
-@@ -278,6 +278,8 @@ module.exports.buildConfig = function buildConfig(para
+@@ -279,6 +279,8 @@ module.exports.buildConfig = function buildConfig(para
              {
                loader: "sass-loader",
                options: {
