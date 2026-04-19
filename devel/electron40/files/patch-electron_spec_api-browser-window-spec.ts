1b5b6da67db3a82b8b17d1e45d1d6e8308539424
Hiroki Tagato <tagattie@gmail.com>
2026-04-19 18:25:53 +0900

    devel/electron40: Update to 40.9.1
    Changelog:
    - https://github.com/electron/electron/releases/tag/v40.9.0
    - https://github.com/electron/electron/releases/tag/v40.9.1

--- devel/electron40/files/patch-electron_spec_api-browser-window-spec.ts
+++ devel/electron40/files/patch-electron_spec_api-browser-window-spec.ts
@@ -1,4 +1,4 @@
---- electron/spec/api-browser-window-spec.ts.orig	2026-03-26 20:19:14 UTC
+--- electron/spec/api-browser-window-spec.ts.orig	2026-04-15 14:05:49 UTC
 +++ electron/spec/api-browser-window-spec.ts
 @@ -69,7 +69,7 @@ describe('BrowserWindow module', () => {
        }).not.to.throw();
@@ -180,7 +180,7 @@
      // Not implemented on Linux.
      afterEach(closeAllWindows);
  
-@@ -6988,7 +6988,7 @@ describe('BrowserWindow module', () => {
+@@ -7008,7 +7008,7 @@ describe('BrowserWindow module', () => {
    describe('"transparent" option', () => {
      afterEach(closeAllWindows);
  
