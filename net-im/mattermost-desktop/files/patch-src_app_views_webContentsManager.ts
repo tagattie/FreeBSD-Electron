b9c9b74ee60f96fcf42130024697a9172f5a3712
Hiroki Tagato <tagattie@gmail.com>
2026-06-18 21:11:18 +0900

    net-im/mattermost-desktop: Update to 6.2.1
    Changelog: https://docs.mattermost.com/product-overview/desktop-app-changelog.html#release-v6-2-extended-support-release

--- net-im/mattermost-desktop/files/patch-src_app_views_webContentsManager.ts
+++ net-im/mattermost-desktop/files/patch-src_app_views_webContentsManager.ts
@@ -1,8 +1,8 @@
---- src/app/views/webContentsManager.ts.orig	2025-11-18 22:10:00 UTC
+--- src/app/views/webContentsManager.ts.orig	2026-06-08 13:42:42 UTC
 +++ src/app/views/webContentsManager.ts
-@@ -64,7 +64,7 @@ export class WebContentsManager {
-         ipcMain.on(UPDATE_SERVER_THEME, this.handleUpdateServerTheme);
-         ipcMain.on(UPDATE_THEME, this.handleUpdateTheme);
+@@ -69,7 +69,7 @@ export class WebContentsManager {
+         ipcMain.on(UPDATE_SERVER_THEME, ipcValidate(this.handleUpdateServerTheme, [themeSchema.required()]));
+         ipcMain.on(UPDATE_THEME, ipcValidate(this.handleUpdateTheme, [themeSchema.required()]));
  
 -        if (process.platform !== 'linux') {
 +        if (process.platform !== 'linux' && process.platform !== 'freebsd') {
