4b2992396f694d4dd4f127e16a61fbf28355c600
Hiroki Tagato <tagattie@gmail.com>
2026-06-21 18:17:47 +0900

    deskutils/superproductivity: Update to 18.12.0
    Changelog:
    - https://github.com/super-productivity/super-productivity/releases/tag/v18.11.0
    - https://github.com/super-productivity/super-productivity/releases/tag/v18.12.0

--- deskutils/superproductivity/files/patch-electron_indicator.ts
+++ deskutils/superproductivity/files/patch-electron_indicator.ts
@@ -1,4 +1,4 @@
---- electron/indicator.ts.orig	2026-05-23 10:53:32 UTC
+--- electron/indicator.ts.orig	2026-06-20 15:17:50 UTC
 +++ electron/indicator.ts
 @@ -59,7 +59,7 @@ const IS_MAC = process.platform === 'darwin';
  let _isListenersInitialized = false;
@@ -8,4 +8,4 @@
 +const IS_LINUX = process.platform === 'linux' || process.platform === 'freebsd';
  const IS_WINDOWS = process.platform === 'win32';
  
- // Stable GUIDs per Windows distribution type.
+ // Stable GUID for the NSIS (installer) build only.
--- deskutils/superproductivity/files/patch-electron_preload.ts
+++ deskutils/superproductivity/files/patch-electron_preload.ts
@@ -1,11 +1,11 @@
---- electron/preload.ts.orig	2026-06-12 11:11:09 UTC
+--- electron/preload.ts.orig	2026-06-20 15:17:50 UTC
 +++ electron/preload.ts
-@@ -107,7 +107,7 @@ const ea: ElectronAPI = {
+@@ -89,7 +89,7 @@ const ea: ElectronAPI = {
      webFrame.setZoomFactor(zoomFactor);
    },
    getZoomFactor: () => webFrame.getZoomFactor(),
 -  isLinux: () => process.platform === 'linux',
 +  isLinux: () => process.platform === 'linux' || process.platform === 'freebsd',
-   isGnomeDesktop,
+   isGnomeDesktop: () => IS_GNOME_DESKTOP,
+   isGnomeWayland: () => IS_GNOME_WAYLAND,
    isMacOS: () => process.platform === 'darwin',
-   isAppleSilicon: () => process.platform === 'darwin' && process.arch === 'arm64',
