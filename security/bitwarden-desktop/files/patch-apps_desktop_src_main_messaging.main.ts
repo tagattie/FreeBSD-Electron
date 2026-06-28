072093ba40da989ad4cbfbc5643bdfefdc817d9a
Hiroki Tagato <tagattie@gmail.com>
2026-06-28 18:01:29 +0900

    security/bitwarden-desktop: Update to 2026.6.0
    Changelog: https://github.com/bitwarden/clients/releases/tag/desktop-v2026.6.0

--- security/bitwarden-desktop/files/patch-apps_desktop_src_main_messaging.main.ts
+++ security/bitwarden-desktop/files/patch-apps_desktop_src_main_messaging.main.ts
@@ -1,4 +1,4 @@
---- apps/desktop/src/main/messaging.main.ts.orig	2026-05-29 16:42:52 UTC
+--- apps/desktop/src/main/messaging.main.ts.orig	2026-06-25 01:41:32 UTC
 +++ apps/desktop/src/main/messaging.main.ts
 @@ -129,7 +129,7 @@ export class MessagingMain {
    }
@@ -7,7 +7,7 @@
 -    if (process.platform === "linux") {
 +    if (process.platform === "linux" || process.platform === "freebsd") {
        if (isFlatpak()) {
-         autostart.setAutostart(true, [AUTOSTART_FLAG]).catch((e) => {});
+         autostart.setAutostart(true, ["bitwarden.sh", AUTOSTART_FLAG, "%U"]).catch((e) => {});
        } else if (isSnapStore()) {
 @@ -140,7 +140,7 @@ export class MessagingMain {
    Version=${app.getVersion()}
