4475e6fd78350894704cfb622a1471713ad5400d
Hiroki Tagato <tagattie@gmail.com>
2026-07-07 08:03:09 +0900

    devel/electron42: Update to 42.6.0
    Changelog:
    - https://github.com/electron/electron/releases/tag/v42.5.2
    - https://github.com/electron/electron/releases/tag/v42.6.0

--- devel/electron42/files/patch-gpu_command__buffer_service_gles2__cmd__decoder.cc
+++ devel/electron42/files/patch-gpu_command__buffer_service_gles2__cmd__decoder.cc
@@ -1,6 +1,6 @@
---- gpu/command_buffer/service/gles2_cmd_decoder.cc.orig	2026-06-24 12:54:01 UTC
+--- gpu/command_buffer/service/gles2_cmd_decoder.cc.orig	2026-07-06 11:02:38 UTC
 +++ gpu/command_buffer/service/gles2_cmd_decoder.cc
-@@ -2866,7 +2866,7 @@ std::unique_ptr<GLES2Decoder> GLES2Decoder::Create(
+@@ -3018,7 +3018,7 @@ std::unique_ptr<GLES2Decoder> GLES2Decoder::Create(
    }
  
  // Allow linux to run fuzzers.
