9dfc3818ac4f2744225000cdb86270e273a6981b
Hiroki Tagato <tagattie@gmail.com>
2026-07-07 13:38:08 +0900

    net-im/rocketchat-desktop: Update to 4.15.2
    Changelog:
    - https://github.com/RocketChat/Rocket.Chat.Electron/releases/tag/4.15.1
    - https://github.com/RocketChat/Rocket.Chat.Electron/releases/tag/4.15.2

--- net-im/rocketchat-desktop/files/patch-src_ui_components_SettingsView_features_FlashFrame.tsx
+++ net-im/rocketchat-desktop/files/patch-src_ui_components_SettingsView_features_FlashFrame.tsx
@@ -1,9 +1,9 @@
---- src/ui/components/SettingsView/features/FlashFrame.tsx.orig	2024-01-30 23:48:22 UTC
+--- src/ui/components/SettingsView/features/FlashFrame.tsx.orig	2026-07-02 22:53:28 UTC
 +++ src/ui/components/SettingsView/features/FlashFrame.tsx
-@@ -53,7 +53,7 @@ export const FlashFrame = (props: FlashFrameProps) => 
-           onChange={handleChange}
-         />
-       </FieldRow>
+@@ -50,7 +50,7 @@ export const FlashFrame = (props: FlashFrameProps) => 
+       onChange={handleChange}
+       className={props.className}
+     >
 -      {process.platform === 'linux' && (
 +      {(process.platform === 'linux' || process.platform === 'freebsd') && (
          <Callout
