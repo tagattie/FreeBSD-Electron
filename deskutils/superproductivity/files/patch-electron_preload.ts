58849c94e09bec708068708edd82cd480a37d31e
Hiroki Tagato <tagattie@gmail.com>
2026-09-14 21:37:31 +0900

    deskutils/superproductivity: Update to 19.0.1
    Changelog: https://github.com/super-productivity/super-productivity/releases/tag/v19.0.1

--- deskutils/superproductivity/files/patch-electron_preload.ts
+++ deskutils/superproductivity/files/patch-electron_preload.ts
@@ -1,6 +1,6 @@
---- electron/preload.ts.orig	2026-07-17 21:17:53 UTC
+--- electron/preload.ts.orig	2026-09-12 10:08:04 UTC
 +++ electron/preload.ts
-@@ -91,7 +91,7 @@ const ea: ElectronAPI = {
+@@ -92,7 +92,7 @@ const ea: ElectronAPI = {
      webFrame.setZoomFactor(zoomFactor);
    },
    getZoomFactor: () => webFrame.getZoomFactor(),
